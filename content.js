b33afa7205f84e8b35adde6073ea5ff000915c30
LouisLanganay <louis.langanay@epitech.eu>
2023-10-19 23:18:09 +0200

    fix: Usefull popup was removed
    - Usefull popup like "Save to playlist" was removed
    - Backdrop was removed with usefull popup
    - Only run script on youtube.com

--- content.js
+++ content.js
@@ -1,14 +1,21 @@
+const youtubeUrl = 'https://www.youtube.com/';
+
 setInterval(() => {
+  if (!window.location.href.startsWith(youtubeUrl))
+    return;
   console.log('%cSearching for popup...', 'color: #6EB8FF');
   const backdrop = document.querySelector('tp-yt-iron-overlay-backdrop');
-  if (backdrop) {
-    console.log('%cBackdrop found and removed !', 'color: #75FF6E');
-    backdrop.remove();
-  }
   const popup = document.querySelector('tp-yt-paper-dialog');
-  if (popup) {
-    console.log('%cBackdrop found and removed !', 'color: #75FF6E');
+  const isAdBlockerPopup = document.querySelector('tp-yt-paper-dialog > ytd-enforcement-message-view-model > div');
+
+  if (popup && isAdBlockerPopup) {
+    console.log('%cPopup found and removed !', 'color: #75FF6E');
     popup.remove();
+
+    if (backdrop) {
+      console.log('%cBackdrop found and removed !', 'color: #75FF6E');
+      backdrop.style.display = 'none';
+    }
 
     const playerButton = document.querySelector('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button');
     if (playerButton) {
